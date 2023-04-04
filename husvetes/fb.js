65769039150b7768a3b69f7e13a8c0db1484a30b
Danika2007 <113986708+Danika2007@users.noreply.github.com>
2023-04-04 12:07:04 +0200

    Add files via upload

---
+++ husvetes/fb.js
@@ -0,0 +1,38 @@
+let eggs =  []
+
+
+function giveMeEgg(){
+let egg = document.createElement("div")
+egg.classList.add("egg")
+
+egg.style.left = Math.floor(Math.random()*window.innerWidth) + "px"
+egg.style.top = (Math.floor(Math.random()*-500)-100) + "px"
+
+
+
+document.body.appendChild(egg)
+console.log("egg created",egg)
+return egg
+}
+
+document.body.appendChild(giveMeEgg())
+
+for (let i = 0; i < 10 ; i++){
+    eggs.push(giveMeEgg())
+}
+
+setInterval(()=>{
+eggs.forEach((egg)=>{
+let newTop = egg.style.top.replace("px","")*1 + 5
+ egg.style.top = newTop + "px"
+
+})
+
+},200)
+
+
+
+
+
+
+
